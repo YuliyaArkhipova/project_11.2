599f54bea34804de3d6e9b4f2a584026554303ad
YuliyaArkhipova <y.arhipova@bk.ru>
2024-02-01 00:41:11 +0300

    'add func_new'

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def func_up(words):
     return words.upper()
+
+def func_up_first_letter(words):
+    """Функция делает заглавной первую букву каждого слова
+    """
+    return words.title()
+
+
+
